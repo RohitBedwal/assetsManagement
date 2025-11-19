283c9e8a481f7a206453ba5e6a83fd92ce6efde1
“rohit” <rohitbedwal09@gmail.com>
2025-11-19 22:39:06 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',             // <-- ADD THIS !!!
+  base: '/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  // This ensures _redirects file is copied to dist folder
+  publicDir: 'public'
 })
